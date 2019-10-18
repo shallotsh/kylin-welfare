6ead1dc167b37cb08bd1cb2eea4fccfa8d2ab27d
shallotsh <shallotsh@gmail.com>
2019-10-18 22:27:02 +0800

    新增删除码取重导出

--- src/main/java/org/kylin/service/exporter/impl/DeletedCodesExporter.java
+++ src/main/java/org/kylin/service/exporter/impl/DeletedCodesExporter.java
@@ -1,9 +1,12 @@
 package org.kylin.service.exporter.impl;
 
+import javafx.util.Pair;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.eclipse.jetty.http.HttpStatus;
 import org.kylin.bean.LabelValue;
 import org.kylin.bean.p5.WCode;
 import org.kylin.bean.p5.WCodeReq;
@@ -15,12 +18,11 @@ import org.kylin.util.DocUtils;
 import org.kylin.util.WCodeUtils;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Component
+@Slf4j
 public class DeletedCodesExporter extends AbstractDocumentExporter {
 
     @Override
@@ -35,6 +37,17 @@ public class DeletedCodesExporter extends AbstractDocumentExporter {
             return;
         }
 
+        if(ExportPatternEnum.getById(data.getExportFormat()).get() == ExportPatternEnum.DELETED_HAS_SAME_BIT_CODES){
+
+            List<WCode> wCodes = getDoubleCheckedCodes(wCodesArray);
+            if(CollectionUtils.isEmpty(wCodes)){
+                return ;
+            }
+            String subTitle = "累计杀码非对子 " + wCodes.size() + " 注: ";
+            printCodes(docHolder.getDocument().createParagraph(), LabelValue.<List<WCode>>builder().data(wCodes).label("累计杀码集合").build(), subTitle);
+            return;
+        }
+
         for(int i=0; i<wCodesArray.size(); i++){
             LabelValue<List<WCode>> labelValue = wCodesArray.get(i);
 //
@@ -44,12 +57,14 @@ public class DeletedCodesExporter extends AbstractDocumentExporter {
 
             List<WCode> nonPairCodes = WCodeUtils.filterNonPairCodes(labelValue.getData());
             LabelValue<List<WCode>> nonPairLabelCodes = LabelValue.<List<WCode>>builder().label(labelValue.getLabel()).data(nonPairCodes).build();
-            printCodes(docHolder.getDocument().createParagraph(), nonPairLabelCodes, i+1, "非对子");
+            String subTitle = "A" + i+1 + "(操作:" + labelValue.getLabel() + ", 杀码非对子" + nonPairCodes.size() + " 注): ";
+            printCodes(docHolder.getDocument().createParagraph(), nonPairLabelCodes, subTitle);
 
         }
     }
 
-    private void printCodes(XWPFParagraph paragraph, LabelValue<List<WCode>> labelValue, Integer index, String desc){
+    @Deprecated
+    private void printCodes(XWPFParagraph paragraph, LabelValue<List<WCode>> labelValue, String subTitle){
 
         Objects.requireNonNull(labelValue);
 
@@ -59,7 +74,7 @@ public class DeletedCodesExporter extends AbstractDocumentExporter {
 
         List<WCode> wCodes = labelValue.getData();
 
-        DocUtils.writeSubTitle(paragraph, "A" + index + "(操作:" + labelValue.getLabel() + ", 杀码" + desc + wCodes.size() + " 注): ");
+        DocUtils.writeSubTitle(paragraph, subTitle);
 
         XWPFRun content = paragraph.createRun();
         content.setFontSize(14);
@@ -75,8 +90,83 @@ public class DeletedCodesExporter extends AbstractDocumentExporter {
         content.addBreak();
     }
 
+    private List<WCode> getDoubleCheckedCodes(List<LabelValue<List<WCode>>> wCodesArr) {
+        if(CollectionUtils.isEmpty(wCodesArr)){
+            return Collections.emptyList();
+        }
+
+        List<List<WCode>> wCodesArray = wCodesArr.stream().map(wCodes -> WCodeUtils.filterNonPairCodes(wCodes.getData())).collect(Collectors.toList());
+
+        Set<WCode> wCodes = new HashSet<>();
+        Set<Pair<Integer, Integer>> pairs = new HashSet<>();
+        int count = 0;
+        for(int i=0; i<wCodesArray.size(); i++){
+            List<WCode> wCodes1 = wCodesArray.get(i);
+            count += wCodes1.size();
+            for(int j=0; j!=i && j<wCodesArray.size(); j++){
+                Pair<Integer, Integer> pair = new Pair<>(i, j);
+                if(pairs.contains(pair)){
+                    continue;
+                }
+                pairs.add(pair);
+                pairs.add(new Pair<>(j,i));
+
+                log.info("计算pair：left={}, right={}", i, j);
+
+                List<WCode> wCodesX = wCodesArray.get(j);
+                wCodes.addAll(getHasRepeatedCodesInLeftThreeBits(wCodes1, wCodesX));
+            }
+        }
+        List<WCode> wCodeList = new ArrayList<>(wCodes);
+        Collections.sort(wCodeList);
+        log.info("合并取重完成 from: {} reduce to {} .", count, wCodeList.size());
+        return wCodeList;
+    }
+
+    private List<WCode> getHasRepeatedCodesInLeftThreeBits(List<WCode> wCodes1,List<WCode> wCodes2){
+        if(CollectionUtils.isEmpty(wCodes1) || CollectionUtils.isEmpty(wCodes2)){
+            return Collections.emptyList();
+        }
+
+        List<WCode> wCodes = new ArrayList<>();
+        for(int i=0; i<wCodes1.size(); i++){
+            for(int j=0; j<wCodes2.size(); j++) {
+                if(hasTwoSameBitCodesAtLeast(wCodes1.get(i), wCodes2.get(j))){
+                    wCodes.add(wCodes1.get(i));
+                    wCodes.add(wCodes2.get(j));
+//                    log.info("Get. left={},right={}", wCodes1.get(i).getString(false, false), wCodes2.get(j).getString(false, false));
+                }
+            }
+        }
+
+        return wCodes;
+    }
+
+
+    /**
+     * 前三位中至少两位相等
+     * @param a
+     * @param b
+     * @return
+     */
+    private boolean hasTwoSameBitCodesAtLeast(WCode a, WCode b){
+        if(a.getDim() != b.getDim()){
+            return false;
+        }
+
+        int count = 0;
+        for(int i= 0; i<a.getDim() && i<3; i++){
+            if(a.getCodes().get(i) == b.getCodes().get(i)){
+                count++;
+            }
+        }
+
+        return count >= 2;
+    }
+
+
     @Override
     public List<ExportPatternEnum> getSupportedExportPatterns() {
-        return Arrays.asList(ExportPatternEnum.DELETED_CODES);
+        return Arrays.asList(ExportPatternEnum.DELETED_CODES, ExportPatternEnum.DELETED_HAS_SAME_BIT_CODES);
     }
 }
