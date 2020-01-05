088292b1d7d40e2ff66bbb6ad58eda9ab007dc44
shallotsh <shallotsh@gmail.com>
2020-01-05 23:14:54 +0800

    修改三码跨度内涵

--- src/main/java/org/kylin/algorithm/filter/impl/RangeFilter.java
+++ src/main/java/org/kylin/algorithm/filter/impl/RangeFilter.java
@@ -1,14 +1,17 @@
 package org.kylin.algorithm.filter.impl;
 
+import com.google.common.collect.Sets;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.kylin.algorithm.filter.CodeFilter;
 import org.kylin.bean.FilterParam;
 import org.kylin.bean.W3DCode;
 import org.kylin.bean.WelfareCode;
+import org.kylin.constant.BitConstant;
 import org.kylin.util.TransferUtil;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -41,12 +44,28 @@ public class RangeFilter implements CodeFilter<WelfareCode> {
         Iterator<W3DCode> iterator = w3DCodes.iterator();
         while(iterator.hasNext()){
             W3DCode w3DCode = iterator.next();
-            int range = w3DCode.getMax() - w3DCode.getMin();
-            if(!rangeSet.contains(range)) {
+            if(!containRanges(rangeSet, w3DCode)) {
                 iterator.remove();
             }
         }
 
         code.setW3DCodes(w3DCodes);
     }
+
+
+    private boolean containRanges(Set<Integer> rangeSet, W3DCode code){
+        if(CollectionUtils.isEmpty(rangeSet) || code == null){
+            return false;
+        }
+
+        HashSet<Integer> codeRanges = new HashSet<>();
+        codeRanges.add(Math.abs(code.getCodes()[BitConstant.UNIT] - code.getCodes()[BitConstant.DECADE]));
+        if(code.getCodes()[BitConstant.HUNDRED] != null){
+            codeRanges.add(Math.abs(code.getCodes()[BitConstant.UNIT] - code.getCodes()[BitConstant.HUNDRED]));
+            codeRanges.add(Math.abs(code.getCodes()[BitConstant.HUNDRED] - code.getCodes()[BitConstant.DECADE]));
+        }
+
+        return rangeSet.stream().anyMatch(range -> codeRanges.contains(range));
+    }
+
 }
