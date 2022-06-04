176b6b4a4eccd5080f176f963c578fa015932d16
shallotsh <shallotsh@gmail.com>
2022-06-04 20:34:56 +0800

    修改单测问题

--- src/test/java/org/kylin/util/OkHttpUtilsTest.java
+++ src/test/java/org/kylin/util/OkHttpUtilsTest.java
@@ -1,14 +1,11 @@
 package org.kylin.util;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.kylin.bean.sd.SdDrawNoticeResult;
-import org.springframework.boot.test.context.SpringBootTest;
-import sun.jvm.hotspot.utilities.Assert;
 
 import java.time.LocalDate;
 import java.util.Optional;
-
-import static org.junit.Assert.*;
 
 public class OkHttpUtilsTest {
 
@@ -16,6 +13,6 @@ public class OkHttpUtilsTest {
     public void getSdDrawNoticeResult() {
         LocalDate drawDate = LocalDate.now().minusDays(1);
         Optional<SdDrawNoticeResult> resultOptional=  OkHttpUtils.getSdDrawNoticeResult(drawDate, drawDate);
-        Assert.that(resultOptional.isPresent(), "查询结果为空");
+        Assert.assertTrue("查询结果为空", resultOptional.isPresent());
     }
 }
