4a4b94ced26b2a86bcc3658f6e32c315fdf85495
shallotsh <shallotsh@gmail.com>
2022-10-29 22:55:28 +0800

    支持2D导出

--- src/main/java/org/kylin/service/exporter/impl/ExpertRecommendDocExporter.java
+++ src/main/java/org/kylin/service/exporter/impl/ExpertRecommendDocExporter.java
@@ -5,14 +5,11 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
-import org.kylin.bean.W3DCode;
-import org.kylin.bean.WelfareCode;
 import org.kylin.bean.p5.WCode;
 import org.kylin.bean.p5.WCodeReq;
 import org.kylin.constant.ExportPatternEnum;
 import org.kylin.service.exporter.AbstractDocumentExporter;
 import org.kylin.service.exporter.DocHolder;
-import org.kylin.util.TransferUtil;
 import org.kylin.util.WCodeUtils;
 import org.springframework.stereotype.Component;
 
@@ -52,15 +49,13 @@ public class ExpertRecommendDocExporter extends AbstractDocumentExporter{
         }
 
 
-        List<W3DCode> w3DCodes = wCodes.stream().map(this::from).collect(Collectors.toList());
-        List<W3DCode> pairCodes = TransferUtil.getPairCodes(w3DCodes);
-        String title = String.format("对子 %d 注", pairCodes.size());
-        writeW3DCodes(docHolder.getDocument().createParagraph(), pairCodes, title);
+        List<WCode> pairCodesNew = WCodeUtils.filterPairCodes(wCodes);
+        String title = String.format("对子 %d 注", pairCodesNew.size());
+        writeWCodesRefactor(docHolder.getDocument().createParagraph(), pairCodesNew, title);
 
-        List<W3DCode> nonPairCodes = TransferUtil.getNonPairCodes(w3DCodes);
-        title = String.format("非对子 %d 注", nonPairCodes.size());
-
-        writeW3DCodes(docHolder.getDocument().createParagraph(), nonPairCodes, title);
+        List<WCode> nonPairCodesNew = WCodeUtils.filterNonPairCodes(wCodes);
+        title = String.format("非对子 %d 注", nonPairCodesNew.size());
+        writeWCodesRefactor(docHolder.getDocument().createParagraph(), nonPairCodesNew, title);
 
     }
 
@@ -86,36 +81,44 @@ public class ExpertRecommendDocExporter extends AbstractDocumentExporter{
 
     }
 
-
-    public void writeW3DCodes( XWPFParagraph paragraph, List<W3DCode> w3DCodes, String title){
-
-        if(CollectionUtils.isEmpty(w3DCodes)){
-            return;
+    private Map<WCode, Integer> getW3DCodeIntStatRefactor(List<WCode> wCodes){
+        if(CollectionUtils.isEmpty(wCodes)){
+            return Collections.emptyMap();
         }
 
+        Map<WCode, Integer> statMap = new HashMap<>();
+        wCodes.forEach(code -> {
+            Integer count = statMap.getOrDefault(code, 0);
+            statMap.put(code, count + 1);
+        });
+
+        return statMap;
+    }
+
+    // =============================== 以下是基于WCode导出数据===========================
+
+
+
+    public void writeWCodesRefactor( XWPFParagraph paragraph, List<WCode> wCodes, String title){
+        if(CollectionUtils.isEmpty(wCodes)){
+            return;
+        }
         writeSubTitle(paragraph, title);
 
         XWPFRun content = paragraph.createRun();
         content.setFontSize(14);
         paragraph.setAlignment(ParagraphAlignment.LEFT);
 
+        boolean printFreq = wCodes.stream().anyMatch(w3DCode -> w3DCode.getFreq()!=0);
 
+        Map<WCode, Integer> stat = getW3DCodeIntStatRefactor(wCodes);
 
-        List<W3DCode> codes = w3DCodes.stream().filter(w3DCode -> w3DCode.getFreq()!=0).collect(Collectors.toList());
-        boolean printFreq = !CollectionUtils.isEmpty(codes);
-
-        Map<W3DCode, Integer> stat = getW3DCodeIntStat(w3DCodes);
-        w3DCodes = new ArrayList<>(stat.keySet());
-        Collections.sort(w3DCodes, WelfareCode::bitSort);
-        for(W3DCode w3DCode : w3DCodes) {
-            String ct;
-            if(printFreq) {
-                ct = w3DCode.toString();
-            }else{
-                ct = w3DCode.toString().substring(3,6);
-            }
-            if(stat.get(w3DCode) > 1){
-                ct += "("+ stat.get(w3DCode) +")    ";
+        List<WCode> temp = new ArrayList<>(stat.keySet());
+        Collections.sort(temp);
+        for(WCode wCode : temp) {
+            String ct = wCode.getString(printFreq, false);
+            if(stat.get(wCode) > 1){
+                ct += "("+ stat.get(wCode) +")    ";
             }else {
                 ct += "        ";
             }
@@ -129,28 +132,6 @@ public class ExpertRecommendDocExporter extends AbstractDocumentExporter{
         sep.setTextPosition(50);
     }
 
-
-    private Map<W3DCode, Integer> getW3DCodeIntStat(List<W3DCode> w3DCodes){
-        if(CollectionUtils.isEmpty(w3DCodes)){
-            return Collections.emptyMap();
-        }
-
-        Map<W3DCode, Integer> statMap = new HashMap<>();
-        w3DCodes.forEach(code -> {
-            Integer count = statMap.getOrDefault(code, 0);
-            statMap.put(code, count + 1);
-        });
-
-        return statMap;
-    }
-
-    private W3DCode from(WCode wCode){
-        return new W3DCode(wCode.getCodes().get(0),
-                wCode.getCodes().get(1),
-                wCode.getCodes().get(2));
-    }
-
-    // =============================== 以下是基于WCode导出数据===========================
 
     private void saveCodesWithFreq(XWPFParagraph paragraph, List<WCode> wCodes, String title){
 
