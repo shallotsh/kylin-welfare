49dd99bb9f2d295a0071689270e7dbca176a7767
shallotsh <shallotsh@gmail.com>
2023-12-24 21:40:06 +0800

    add

--- src/main/java/org/kylin/api/SdDrawApiController.java
+++ src/main/java/org/kylin/api/SdDrawApiController.java
@@ -6,15 +6,19 @@ import org.kylin.bean.WyfDataResponse;
 import org.kylin.bean.WyfErrorResponse;
 import org.kylin.bean.WyfResponse;
 import org.kylin.bean.sd.SdDrawNoticeResult;
+import org.kylin.constant.ESIndexEnum;
 import org.kylin.util.MyDateUtil;
 import org.kylin.util.OkHttpUtils;
+import org.kylin.wrapper.ESWrapper;
 import org.kylin.wrapper.GuavaCacheWrapper;
+import org.kylin.wrapper.bo.SdDrawResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.Optional;
@@ -28,6 +32,9 @@ public class SdDrawApiController {
 
     @Autowired
     private GuavaCacheWrapper<SdDrawNoticeResult> cacheWrapper;
+
+    @Resource
+    private ESWrapper esWrapper;
 
     @RequestMapping(value = "/draw/notice", method = RequestMethod.GET)
     public WyfResponse findDrawNotice(String name, Integer issueCount){
@@ -45,6 +52,13 @@ public class SdDrawApiController {
         SdDrawNoticeResult result = null;
         try {
             result = cacheWrapper.get(key, () -> OkHttpUtils.getSdDrawNoticeResult(drawDate, drawDate).orElse(null));
+
+            // 记录结果
+            SdDrawResult res = SdDrawResult.from(result.getResult().get(0));
+            if(!esWrapper.exists(ESIndexEnum.WELFARE_RESULT.getIndex(), res.getCode())) {
+                esWrapper.index(ESIndexEnum.WELFARE_RESULT.getIndex(), res.getCode(), res);
+            }
+
             return Optional.ofNullable(result)
                     .map(ret -> new WyfDataResponse<>(ret))
                     .orElse(new WyfErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),
