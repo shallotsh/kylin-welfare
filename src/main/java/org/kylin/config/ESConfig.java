6af8208a741c8b83ca85a0cca6337912d1bcde86
shallotsh <shallotsh@gmail.com>
2024-11-06 22:04:13 +0800

    update credentials

--- src/main/java/org/kylin/config/ESConfig.java
+++ src/main/java/org/kylin/config/ESConfig.java
@@ -4,6 +4,8 @@ import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
+
+import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
@@ -12,22 +14,36 @@ import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder.HttpClientConfigCallback;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ESConfig {
 
+    @Value("${es.host}")
+    private String hostDefault;
+    @Value("${es.port}")
+    private Integer portDefault;
+    @Value("${es.pwd}")
+    private String pwdDefault;
+
 
     @Bean
     public ElasticsearchClient esClient() {
 
+        String hostInner = System.getenv("ES_HOST");
+        String portInner = System.getenv("ES_PORT");
+        String userInner = System.getenv("ES_USR");
+        String credentialsKeyInner = System.getenv("ES_PWD");
+
         final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(AuthScope.ANY,
-                new UsernamePasswordCredentials("elastic", "zCPY7OlZEJ5224dMqtho"));
+                new UsernamePasswordCredentials(userInner != null ? userInner:"elastic",
+                     credentialsKeyInner != null ? credentialsKeyInner: pwdDefault));
 
         RestClient restClient = RestClient
-                .builder(new HttpHost("10.7.97.73", 7200, "http")
+                .builder(new HttpHost(hostInner != null ? hostInner : hostDefault,  portInner != null ? NumberUtils.toInt(portInner): portDefault, "http")
                 ).setHttpClientConfigCallback(new HttpClientConfigCallback() {
                     @Override
                     public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
