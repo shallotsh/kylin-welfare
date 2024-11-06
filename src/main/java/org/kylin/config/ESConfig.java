b7b6d80c97ff7b3a47dbcab6a66cba8424a94cde
shallotsh <shallotsh@gmail.com>
2024-11-06 19:00:44 +0800

    add credentials

--- src/main/java/org/kylin/config/ESConfig.java
+++ src/main/java/org/kylin/config/ESConfig.java
@@ -5,7 +5,13 @@ import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder.HttpClientConfigCallback;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,9 +22,19 @@ public class ESConfig {
     @Bean
     public ElasticsearchClient esClient() {
 
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials("elastic", "zCPY7OlZEJ5224dMqtho"));
+
         RestClient restClient = RestClient
                 .builder(new HttpHost("10.7.97.73", 7200, "http")
-                ).setRequestConfigCallback( builder -> {
+                ).setHttpClientConfigCallback(new HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+                        return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                    }
+                })
+                .setRequestConfigCallback( builder -> {
                     builder.setConnectTimeout(1000);
                     builder.setSocketTimeout(3000);
                     return builder;
