8dd9e84e50c90fc9f42d4234c5034455e9f817ac
shallotsh <shallotsh@gmail.com>
2025-03-21 00:49:33 +0800

    新增本地读取es配置

--- src/main/java/org/kylin/config/ESConfig.java
+++ src/main/java/org/kylin/config/ESConfig.java
@@ -6,6 +6,7 @@ import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
@@ -15,13 +16,14 @@ import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder.HttpClientConfigCallback;
 import org.kylin.util.OkHttpUtils;
-import org.kylin.wrapper.ESWrapper;
 import org.kylin.wrapper.bo.ESClusterConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.File;
 import java.util.Optional;
+import java.util.Scanner;
 
 @Configuration
 @Slf4j
@@ -70,6 +72,15 @@ public class ESConfig {
 
 
     private Optional<ESClusterConfiguration> esClusterConfiguration() {
+
+        // 1.从本地配置文件获取配置：先判断本地文件是否存在，再读取文件内容，反序列化为ESClusterConfiguration对象
+        String configStr = getConfigurationContent();
+        if(StringUtils.isNotBlank(configStr)){
+            ESClusterConfiguration esClusterConfiguration = JSON.parseObject(configStr, ESClusterConfiguration.class);
+            return Optional.of(esClusterConfiguration);
+        }
+
+        // 2.获取远程配置
         try {
             return OkHttpUtils.doGet(configurationUrl, ESClusterConfiguration.class, null);
         } catch (Exception e) {
@@ -77,4 +88,25 @@ public class ESConfig {
         }
         return Optional.empty();
     }
+
+    private String getConfigurationContent() {
+        String userDir = System.getProperty("user.home");
+        String filePath =  userDir + "/.zx_config/wyf_es_config.json";
+        File configFile = new File(filePath);
+        if(configFile.exists()){
+            try {
+                Scanner scanner = new Scanner(configFile);
+                StringBuilder sb = new StringBuilder();
+                while (scanner.hasNextLine()){
+                    sb.append(scanner.nextLine());
+                }
+                scanner.close();
+                return sb.toString();
+            } catch (Exception e) {
+                log.error("read es cluster configuration file error, file:{}", filePath, e);
+            }
+        }
+        return null;
+    }
+
 }
